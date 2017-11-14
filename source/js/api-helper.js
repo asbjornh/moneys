399fb91d7c5bb37095171427064a69f3cca9d156
Alexander Nilsson <alexander@alxoptr.se>
2017-11-14 12:16:26 +0100

    Add support for Bitcoin (BTCUSD=X)

--- source/js/api-helper.js
+++ source/js/api-helper.js
@@ -136,11 +136,12 @@ function getStockData({ symbol, purchasePrice, qty, id }) {
         const data = get(json, "quoteSummary.result[0]");
 
         if (data) {
+          const longName = get(data, "price.longName") || get(data, "price.shortName");
           resolve({
             currency: get(data, "price.currency"),
             currencySymbol: get(data, "price.currencySymbol"),
             id,
-            longName: get(data, "price.longName").replace(/&amp;/g, "&"),
+            longName: longName.replace(/&amp;/g, "&"),
             price: get(data, "price.regularMarketPrice"),
             purchasePrice: parseFloat(purchasePrice),
             qty: parseInt(qty),
