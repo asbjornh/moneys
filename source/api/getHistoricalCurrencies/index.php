86e56521ca9b6309374fc17e29d9e0c6d5ec7cfa
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2017-12-17 17:08:42 +0100

    removed unused endpoint

--- source/api/getHistoricalCurrencies/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  error_reporting(0);
-  include "../vars.php";
-
-  $date = $_REQUEST["date"];
-  $appId = $_REQUEST["appId"];
-
-  echo file_get_contents($historicalCurrenciesUrl . $date . ".json?app_id=" . $appId);
-?>
--- source/api/vars.php
+++ source/api/vars.php
@@ -2,7 +2,6 @@
 	$currencyPriceUrl = "https://api.coinbase.com/v2/prices/";
 	$currencyListUrl = "https://api.coinbase.com/v2/currencies";
 	$exchangeRatesUrl = "https://api.coinbase.com/v2/exchange-rates";
-	$historicalCurrenciesUrl = "https://openexchangerates.org/api/historical/";
 	$openExchangeRatesBaseUrl = "https://openexchangerates.org/api/latest.json";
 	$stockUrl = "https://query2.finance.yahoo.com/v10/finance/quoteSummary/";
 
